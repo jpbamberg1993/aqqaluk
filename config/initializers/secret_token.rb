30f850c0cf0de8d2ea0305e8effe2af6fdab2f0f
Chris Maxwell <cmaxwell@ojala.com>
2013-10-30 15:23:44 -0400

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Aqqaluk::Application.config.secret_key_base = 'a8715ccc47c22e24720f948c276f1c33d61124db314589296e842180ffc24e211bd9cb96c78307b5a5f04d491ea03f7d37012767c4d0506477f960da283328e0'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Aqqaluk::Application.config.session_store :cookie_store, key: '_aqqaluk_session'
