d0102bce541c076e0d542ea1a9318e1384225afe
Chris Maxwell <cmaxwell@ojala.com>
2014-01-01 12:24:54 -0500

    Add jQuery anystretch, configure, and comment out unnecessary function causing problems.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,7 +10,9 @@
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require jquery
-//= require jquery_ujs
+//=# require jquery
+//=# require jquery_ujs
+//= require jquery-2.0.3.min
 //= require turbolinks
+//= require jquery.anystretch
 //= require site
--- app/assets/javascripts/site.js
+++ app/assets/javascripts/site.js
@@ -1,10 +1,19 @@
+// ===========================
 // Remove Anchor Outline
+// ===========================
 
 // http://stackoverflow.com/questions/1535538/jquery-getting-rid-of-dotted-outline-around-buttons
 // Not recommended: http://css-tricks.com/removing-the-dotted-outline/
 
-$(function() {
-    $('.autoblur').live("click", function(event) {
-        this.blur();
-    });
-});
+// $(function() {
+//     $('.autoblur').live("click", function(event) {
+//         this.blur();
+//     });
+// });
+
+
+// ===========================
+// Anystretch
+// ===========================
+
+$('#hero').anystretch("assets/heroes/aqqaluk.jpg", {speed: 200, positionY: 'top'});
