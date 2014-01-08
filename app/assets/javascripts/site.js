996c4d7232fd52e901ec49ea44a3ae874f574425
Chris Maxwell <cmaxwell@ojala.com>
2014-01-07 21:53:16 -0500

    Make hero slides responsive.

--- app/assets/javascripts/site.js
+++ app/assets/javascripts/site.js
@@ -79,6 +79,56 @@ $("#carousel2").carouFredSel({
     }
 });
 
+$('#carousel-b').carouFredSel({
+    width               : "100%",
+    // height              : 487,
+    items: {
+        visible         : 1,
+        // width           : 487,
+        // height          : 272
+    },
+    scroll: {
+        items           : 1,
+        duration        : 1000,
+        // timeoutDuration : 7000,
+        // pauseOnHover    : true
+
+        onBefore: function(data) {
+          // hide current block
+          data.items.old.add( data.items.visible ).find( '.slide-block' ).stop().fadeOut();
+        },
+
+        onAfter: function(data) {
+          // show active slide block
+          data.items.visible.last().find( '.slide-block' ).stop().fadeIn(1500);
+        }
+    },
+
+    onCreate: function(data){
+      // show first title block and hide the rest
+      $(this).find( '.slide-block' ).hide();
+      $(this).find( '.slide.active .slide-block' ).stop().fadeIn();
+    },
+
+    auto                : false,
+    prev: {
+        button          : "#prev",
+        key             : "left"
+    },
+    next: {
+        button          : "#next",
+        key             : "right"
+    },
+    pagination: {
+        container       : "#pager"
+    },
+    swipe               : true
+    // swipe: {
+    //   onMouse           : true,
+    //   onTouch           : true
+    // }
+    // cookie              : true
+});
 
 // ===========================
 // Anystretch
