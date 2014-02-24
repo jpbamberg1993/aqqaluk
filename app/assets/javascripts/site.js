746347faa54852e322b98e36ee8f92d22b5728eb
Chris Maxwell <cmaxwell@ojala.com>
2014-02-23 20:49:41 -0500

    Fix text fade in and out.

--- app/assets/javascripts/site.js
+++ app/assets/javascripts/site.js
@@ -156,7 +156,7 @@ $(document).ready(function(){
     speed: 1000,                   // speed you want the toggle to happen
     easing: '',                    // the animation effect you want. Remove this line if you dont want an effect and if you haven't included jQuery UI
     changeText: 1,                 // if you dont want the button text to change, set this to 0
-    showText: 'Show more',         // the button text to show when a div is closed
+    showText: 'Read more',         // the button text to show when a div is closed
     hideText: 'Show less'          // the button text to show when a div is open
   });
 
