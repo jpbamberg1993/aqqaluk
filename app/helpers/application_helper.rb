1a0643f42cbc787231c0d4bdf14d27d524922cbe
Chris Maxwell <cmaxwell@ojala.com>
2014-03-02 21:20:51 -0500

    Hook up navigation.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -12,4 +12,14 @@ module ApplicationHelper
   def browsehappy
     render :partial => 'layouts/browsehappy'
   end
+
+  def nav_location(title, url, options = {})
+    current_nav = options.delete(:current)
+    options[:class] = (current_nav == title) ? 'active' : 'inactive'
+    content_tag(:li, link_to(title, url), options)
+  end
+
+  def currently_at(location)
+    render partial: 'shared/navigation', locals: {current_nav: location}
+  end
 end
