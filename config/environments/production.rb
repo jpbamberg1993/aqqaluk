f4a035eca10a752e4b0b02dd7592a1eb78e9f318
Chris Maxwell <cmaxwell@ojala.com>
2014-03-18 15:04:04 -0400

    Add prefetch, prepare production.rb with commented line for future use, change text in homepage.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -58,6 +58,7 @@ Aqqaluk::Application.configure do
   # config.action_controller.asset_host = "http://assets.example.com"
   # config.action_controller.asset_host = "//#{ENV['FOG_DIRECTORY']}.s3.amazonaws.com"
   config.action_controller.asset_host = "http://d2rfidc3jzjxi6.cloudfront.net"
+  # config.action_controller.asset_host = "http://cdn%d.aqqaluktrust.com"
 
   # Precompile additional assets.
   # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
