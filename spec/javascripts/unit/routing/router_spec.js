93e42994ef246169d8691ed2ca9c9c610e4f686e
Chris Maxwell <cmaxwell@ojala.com>
2014-04-10 14:51:39 -0400

    Add first failing spec; root route.

--- spec/javascripts/unit/routing/router_spec.js
+++ spec/javascripts/unit/routing/router_spec.js
@@ -1,8 +1,16 @@
-module( "Routing specs", {
+module('Routing specs', {
     setup: function() {
 
     },
     teardown: function() {
-      AddressBook.reset();
+      Aqqaluk.reset();
     }
+});
+
+test('root route', function () {
+    visit('/');
+    andThen(function () {
+      var current_route = Aqqaluk.__container__.lookup('controller:application').currentRouteName;
+      equal(current_route, '', 'Expected ****** got: ' + current_route);
+    });
 });
