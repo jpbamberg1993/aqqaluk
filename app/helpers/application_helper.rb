b1424afa8ee29e822c45072fb5ed515b677914b1
Chris Maxwell <cmaxwell@ojala.com>
2015-09-16 07:47:48 -0700

    Remove old navigation style

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -12,14 +12,4 @@ module ApplicationHelper
   def browsehappy
     render :partial => 'layouts/browsehappy'
   end
-
-  def nav_location(title, url, options = {})
-    current_nav = options.delete(:current)
-    options[:class] = (current_nav == title) ? 'active' : 'inactive'
-    content_tag(:li, link_to(title, url), options)
-  end
-
-  def currently_at(location)
-    render partial: 'layouts/navigation', locals: {current_nav: location}
-  end
 end
