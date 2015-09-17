72339f962d848352f7e21ee1b15ea7e83b91969d
Chris Maxwell <cmaxwell@ojala.com>
2015-09-17 13:14:36 -0700

    Update all routes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,24 +1,24 @@
 Aqqaluk::Application.routes.draw do
   root :to => 'pages#home'
 
-  get "pages/home"
-  get "pages/home_v2"
-  get "pages/about_us"
-  get "pages/news"
-  get "pages/founder"
-  get "pages/board_members"
-  get "pages/staff"
-  get "pages/education"
-  get "pages/students_v1"
-  get "pages/savaktuq"
-  get "pages/language"
-  get "pages/language_v1"
-  get "pages/camp"
-  get "pages/support"
-  get "pages/golf_challenge"
-  get "pages/memorial_fund"
+  get '/home',           to: 'pages#home'
+  get '/home_v2',        to: 'pages#home_v2'
+  get '/about_us',       to: 'pages#about_us'
+  get '/news',           to: 'pages#news'
+  get '/founder',        to: 'pages#founder'
+  get '/board_members',  to: 'pages#board_members'
+  get '/staff',          to: 'pages#staff'
+  get '/education',      to: 'pages#education'
+  get '/students_v1',    to: 'pages#students_v1'
+  get '/savaktuq',       to: 'pages#savaktuq'
+  get '/language',       to: 'pages#language'
+  get '/language_v1',    to: 'pages#language_v1'
+  get '/camp',           to: 'pages#camp'
+  get '/support',        to: 'pages#support'
+  get '/golf_challenge', to: 'pages#golf_challenge'
+  get '/memorial_fund',  to: 'pages#memorial_fund'
 
   comfy_route :cms_admin, :path => '/admin'
   # Make sure this routeset is defined last
-  comfy_route :cms, :path => '/test', :sitemap => false
+  comfy_route :cms, :path => '/', :sitemap => false
 end
