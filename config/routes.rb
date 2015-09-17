7b6dd2c15f8962cdd006ddb48ad465a3c6354683
Chris Maxwell <cmaxwell@ojala.com>
2015-09-17 12:24:43 -0700

    Update CMS route.

--- config/routes.rb
+++ config/routes.rb
@@ -20,5 +20,5 @@ Aqqaluk::Application.routes.draw do
 
   comfy_route :cms_admin, :path => '/admin'
   # Make sure this routeset is defined last
-  comfy_route :cms, :path => '/', :sitemap => false
+  comfy_route :cms, :path => '/test', :sitemap => false
 end
