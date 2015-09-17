91e34317bf85766e54f0d1925b964f5e72241d53
Chris Maxwell <cmaxwell@ojala.com>
2015-09-17 07:19:16 -0700

    Fix class conflict and install navigation javascript.

--- app/assets/javascripts/site.js
+++ app/assets/javascripts/site.js
@@ -144,6 +144,29 @@ function textSlide(){
 
 
 // ===========================
+// Drop Down Menu
+// ===========================
+
+$(function(){
+
+    $("ul.dropdown li").hover(function(){
+
+        $(this).addClass("hover");
+        $('ul:first',this).css('visibility', 'visible');
+
+    }, function(){
+
+        $(this).removeClass("hover");
+        $('ul:first',this).css('visibility', 'hidden');
+
+    });
+
+    // $("ul.dropdown li ul li:has(ul)").find("a:first").append(" &raquo; ");
+
+});
+
+
+// ===========================
 // Show Hide
 // ===========================
 
