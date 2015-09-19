3eb7c9f6fcdb30c52dd90bedb04532384206bd26
Chris Maxwell <cmaxwell@ojala.com>
2015-09-19 13:28:28 -0700

    Change routes to match CMS.

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,10 @@ Aqqaluk::Application.routes.draw do
 
   get '/home',           to: 'pages#home'
   get '/home_v2',        to: 'pages#home_v2'
-  get '/about_us',       to: 'pages#about_us'
+  get '/about-us',       to: 'pages#about_us',       as: :about_us
   get '/news',           to: 'pages#news'
   get '/founder',        to: 'pages#founder'
-  get '/board_members',  to: 'pages#board_members'
+  get '/board-members',  to: 'pages#board_members',  as: :board_members
   get '/staff',          to: 'pages#staff'
   get '/education',      to: 'pages#education'
   get '/students_v1',    to: 'pages#students_v1'
@@ -17,8 +17,8 @@ Aqqaluk::Application.routes.draw do
   get '/language_v1',    to: 'pages#language_v1'
   get '/camp',           to: 'pages#camp'
   get '/support',        to: 'pages#support'
-  get '/golf_challenge', to: 'pages#golf_challenge'
-  get '/memorial_fund',  to: 'pages#memorial_fund'
+  get '/golf-challenge', to: 'pages#golf_challenge', as: :golf_challenge
+  get '/memorial-fund',  to: 'pages#memorial_fund',  as: :memorial_fund
 
   comfy_route :cms_admin, :path => '/admin'
   # Make sure this routeset is defined last
