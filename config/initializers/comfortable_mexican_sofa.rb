e35bb96e5da38227863747c72bdb30615b74a59b
Chris Maxwell <cmaxwell@ojala.com>
2015-09-19 11:18:34 -0700

    Change admin title.

--- config/initializers/comfortable_mexican_sofa.rb
+++ config/initializers/comfortable_mexican_sofa.rb
@@ -2,7 +2,7 @@
 
 ComfortableMexicanSofa.configure do |config|
   # Title of the admin area
-  #   config.cms_title = 'ComfortableMexicanSofa CMS Engine'
+    config.cms_title = 'Aqqaluk Trust - Admin'
 
   # Controller that is inherited from CmsAdmin::BaseController
   #   config.base_controller = 'ApplicationController'
